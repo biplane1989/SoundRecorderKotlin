9051d8f9536512d1b161b5861ffa5d2ec40d86e0
biplane1989 <33828788+biplane1989@users.noreply.github.com>
2019-09-27 02:05:00 +0700

    do my home

--- app/src/main/java/com/example/soundrecorderkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/soundrecorderkotlin/view/MainActivity.kt
@@ -1,12 +1,17 @@
-package com.example.soundrecorderkotlin
+package com.example.soundrecorderkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import com.example.soundrecorderkotlin.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.fra_main).navigateUp()
     }
 }
